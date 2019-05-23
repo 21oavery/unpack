7c4449ec8d5e4f8d5066c0acbac7bff750e6ba94
powerboat9 <powerboat9.gamer@gmail.com>
2019-05-22 21:48:35 -0400

    Pushing changes

--- Makefile
+++ Makefile
@@ -1,1 +1,8 @@
-all: libfinder.c
+main.bin: loader.o
+	ld --oformat binary loader.o -o main.bin
+
+*.o: *.c
+	gcc -nostdlib -fPIC -o $@ -c $<
+
+clean:
+	rm *.o main.bin
--- loader.c
+++ loader.c
@@ -7,23 +7,26 @@
 
 void *getKern() {
     void *ptr;
+    SAB;
+    SAB;
     // Get peb
 #ifdef _LP64
-    __asm__("movq fs:0x60, %0":"=r"(ptr));
+    __asm__("movq %%fs:0x60, %0":"=r"(ptr));
 #else
-    __asm__("movl fs:0x30, %0":"=r"(ptr));
+    __asm__("movl %%fs:0x30, %0":"=r"(ptr));
 #endif
     // Get peb_ltr_data
     ptr = ((char *) ptr) + (4 + 2 * sizeof(void *));
-    ptr = *((void *) ptr);
+    ptr = *((void **) ptr);
     // Get InMemoryOrderModuleList
     ptr = ((char *) ptr) + (8 + 3 * sizeof(void *));
     ptr = *((void **) ptr);
     // Get third item (kernal32.dll) base address
+    SAB;
+    SAB;
     return ***((char ****) ptr) + (4 * sizeof(void *));
 }
 
-int main(int argc, char **argv) {
-    SAB;
-    
+__attribute__((section(".text.startup"))) void _start() {
+    void *kern = getKern();
 }
