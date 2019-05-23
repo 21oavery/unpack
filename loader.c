4b9195de0df5aaca79c8c65543e842bc70e0e744
powerboat9-school <33042784+powerboat9-school@users.noreply.github.com>
2019-05-23 14:04:16 -0400

    Update loader.c

--- loader.c
+++ loader.c
@@ -37,31 +37,37 @@ char _access(unsigned int offset) {
 
 #define access(x) _access(((x) + 17) ^ 0xfeedfeed)
 
-void memcpy(restrict void *src, restrict void *dst, unsigned int len) {
+void memcpy(const restrict char *src, restrict void *dst, unsigned int len) {
     for (unsigned int i = 0; i < len; i++) {
-        ((char *) dst)[i] = ((char *) src)[i];
+        dst[i] = src[i];
     }
 }
 
-int streq(const void *a, const void *b) {
+int streq(const char *a, const char *b) {
     unsigned int i = 0;
     while (1) {
-        char r = ((char *) a)[i];
-        if (r != ((char *) b)[i]) return 0;
+        char r = a[i];
+        if (r != b[i]) return 0;
         if (!r) return 1;
     }
 }
 
-void *getFunct(void *dll) {
+void *getExportTable(void *dll) {
     // Get start of new exe header
-    void *ptr = ((char *) dll) + 60;
-    ptr = ((char *) dll) + *((long *) ptr);
-    // Get export table
-    ptr = ((char *) ptr) + 120;
-    unsigned long extRel = *((unsigned long *) ptr);
+    char *ptr = dll + 60;
+    ptr = dll + *((long *) ptr);
+    // Find the number of rva entries
+    ptr += 116;
+    unsigned long relCnt = *((unsigned long *) ptr);
+    if (!relCnt) return NULL;
+    // Get export table rva
+    ptr += 4;
+    unsigned long exportRva = *((unsigned long *) ptr);
     // Find the section the export table is located in
-    ptr = 
+    ptr += relCnt * 8;
+    
 
 __attribute__((section(".text.startup"))) void _start() {
     void *kern = getKern();
+    void *expTable = get
 }
