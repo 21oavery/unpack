00bdbf1689cb8f6321e18c4a7882c4f728854ec5
powerboat9-school <33042784+powerboat9-school@users.noreply.github.com>
2019-05-23 11:32:34 -0400

    Update loader.c

--- loader.c
+++ loader.c
@@ -27,6 +27,41 @@ void *getKern() {
     return ***((char ****) ptr) + (4 * sizeof(void *));
 }
 
+static char strLookup[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.";
+
+char _access(unsigned int offset) {
+    SAB;
+    offset ^= 0xfeedfeed;
+    return strData[offset - 17];
+}
+
+#define access(x) _access(((x) + 17) ^ 0xfeedfeed)
+
+void memcpy(restrict void *src, restrict void *dst, unsigned int len) {
+    for (unsigned int i = 0; i < len; i++) {
+        ((char *) dst)[i] = ((char *) src)[i];
+    }
+}
+
+int streq(const void *a, const void *b) {
+    unsigned int i = 0;
+    while (1) {
+        char r = ((char *) a)[i];
+        if (r != ((char *) b)[i]) return 0;
+        if (!r) return 1;
+    }
+}
+
+void *getFunct(void *dll) {
+    // Get start of new exe header
+    void *ptr = ((char *) dll) + 60;
+    ptr = ((char *) dll) + *((long *) ptr);
+    // Get export table
+    ptr = ((char *) ptr) + 120;
+    unsigned long extRel = *((unsigned long *) ptr);
+    // Find the section the export table is located in
+    ptr = 
+
 __attribute__((section(".text.startup"))) void _start() {
     void *kern = getKern();
 }
